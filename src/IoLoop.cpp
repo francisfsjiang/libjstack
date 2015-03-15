7935cd08bc6587752d29afccd819fb76a51bac44
Neveralso <neveralso@gmail.com>
2015-03-15 23:49:40 +0800

    commit

---
+++ src/IoLoop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Neveralso on 15/3/15.
+//
+
+#include "IoLoop.h"
---
+++ src/IoLoop.h
@@ -0,0 +1,16 @@
+//
+// Created by Neveralso on 15/3/15.
+//
+
+#ifndef _DEMONIAC_IOLOOP_H_
+#define _DEMONIAC_IOLOOP_H_
+
+namespace dc {
+    class IoLoop {
+
+    };
+
+}
+
+
+#endif //_DEMONIAC_IOLOOP_H_
