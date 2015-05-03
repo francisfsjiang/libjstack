d0cea6d02f7bdd5463e6a384a8e673d3ea68f55c
Neveralso <neveralso@gmail.com>
2015-05-03 23:51:19 +0800

    building http server

---
+++ include/demoniac/http/http_header.h
@@ -0,0 +1,23 @@
+//
+// Created by Neveralso on 15/5/2.
+//
+
+
+#ifndef DEMONIAC_HTTP_HTTPHEADER_H
+#define DEMONIAC_HTTP_HTTPHEADER_H
+
+
+namespace demoniac{
+namespace http{
+
+
+class HTTPHeader{
+
+};
+
+
+}
+}
+
+
+#endif //DEMONIAC_HTTP_HEADER_H
